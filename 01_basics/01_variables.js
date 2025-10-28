7c50095a6c6769833878b37fe8c6098cd95550aa
challasaicharanreddy <challasaicharan3@gmail.com>
2025-10-28 22:44:45 +0000

    new files aboutvariables and data types are added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=32155
+let gmail="challasaicharan9@gamil.com"
+var account_Password="121341"
+
+currentCity="Hyderabad"
+
+let state
+/*
+Do not Use var because of the issue in block scope and functional scope
+*/
+
+currentCity="Raipur"
+gmail="cscr@cscr.com"
+account_Password="14322"
+console.table([accountId,gmail,account_Password,currentCity,state])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js codes as newer version
+
+// alert(3 + 3)  //will not print 6 as we are not using browser we are using nodejs
+
+console.log(3 + 3)
+
+let name=("Charan")
+let age=19
+let isLoggedIn= false
+
+//number => 2 to the power 53
+//bigint
+//string=""
+//boolean =>true/false
+//null =>standalone value
+//undefined
+//symbol => uniqueness
+
+console.log(typeof null);   //object
+console.log(typeof undefined);  //undefined
+
+
